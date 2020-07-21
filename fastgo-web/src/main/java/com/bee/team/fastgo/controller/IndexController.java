d3408550fc22fcbb0d4403e6c8ba4cfbefbdb3ba
root <restart@2020>
2020-07-21 19:07:19 +0800

    init

--- fastgo-web/src/main/java/com/bee/team/fastgo/controller/IndexController.java
+++ fastgo-web/src/main/java/com/bee/team/fastgo/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.bee.team.fastgo.controller;
 
 import com.spring.simple.development.core.annotation.base.NoLogin;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +17,15 @@ import java.io.IOException;
  * @Date 2020/4/10/010 19:14
  * @Description //TODO
  **/
+@Api(tags = "帮助")
 @RestController
 public class IndexController {
 
+
     @NoLogin
-    @RequestMapping("/noPermission")
-    public String noPermission() {
-        return "noPermission";
+    @ApiParam(value = "获取帮助")
+    @RequestMapping(value = "/help",method = RequestMethod.GET)
+    public String help() {
+        return "Only oneself can help oneself";
     }
 }
