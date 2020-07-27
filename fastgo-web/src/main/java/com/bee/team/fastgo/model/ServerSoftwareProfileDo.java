d9554a7ad9bfb55286766de9a7bb07c0d58da5a7
MyGitHubNumber <1945282561@qq.com>
2020-07-27 10:16:53 +0800

    提供执行脚本的接口

---
+++ fastgo-web/src/main/java/com/bee/team/fastgo/model/ServerSoftwareProfileDo.java
@@ -0,0 +1,39 @@
+package com.bee.team.fastgo.model;
+
+import com.alibaba.lava.base.LavaDo;
+import lombok.Data;
+
+/**
+ * @description MyBatis Generator 自动创建,对应数据表为：t_server_software_profile
+ *
+ * @author liko
+ * @date   2020/07/27
+ */
+@Data
+public class ServerSoftwareProfileDo extends LavaDo {
+    /**
+     * 服务器IP
+
+     */
+    private String serverIp;
+
+    /**
+     * 软件唯一标识(参考字典)
+     */
+    private String softwareCode;
+
+    /**
+     * 软件名
+     */
+    private String softwareName;
+
+    /**
+     * 软件环境元配置
+     */
+    private String softwareConfig;
+
+    @Override
+    public String getBoQualifiedIntfName() {
+        return "com.bee.team.fastgo.service.server.ServerSoftwareProfileBo";
+    }
+}
