85513d7a82d77ebdeb85994172d58ec6a18fcc3d
root <restart@2020>
2020-07-30 17:36:02 +0800

    bug fixed

--- fastgo-tools/src/main/java/com/bee/team/fastgo/tools/deploy/DeployHandler.java
+++ fastgo-tools/src/main/java/com/bee/team/fastgo/tools/deploy/DeployHandler.java
@@ -56,17 +56,18 @@ public class DeployHandler {
         invokeBuildVue(projectPath);
         String localPath = projectPath + "/dist/";
         if (isWindows()) {
-            localPath = System.getProperties().getProperty("user.dir").substring(0, 2) + projectPath + "/dist/";;
+            localPath = System.getProperties().getProperty("user.dir").substring(0, 2) + projectPath + "/dist/";
+            ;
         }
-        File file = new File(projectPath+"/dist.tar.gz");
-        if(file.exists()){
+        File file = new File(projectPath + "/dist.tar.gz");
+        if (file.exists()) {
             file.delete();
         }
-        GzipUtil.compress(localPath, projectPath+"/dist.tar.gz");
+        GzipUtil.compress(localPath, projectPath + "/dist.tar.gz");
         // 4 推送
         Scp.uploadFile(deployDTO.getServiceIp(), deployDTO.getServicePort().intValue(), deployDTO.getServiceUserName(), deployDTO.getServiceUserPassword(), file, projectPath);
         // 5 部署
-        Scp.vueDeploy(deployDTO.getProjectName(),deployDTO.getProjectPort(), deployDTO.getServicePort(), deployDTO.getServiceIp(), deployDTO.getServiceUserName(), deployDTO.getServiceUserPassword(), projectPath, deployDTO.getSimpleServiceUrl());
+        Scp.vueDeploy(deployDTO.getProjectName(), deployDTO.getProjectPort(), deployDTO.getServicePort(), deployDTO.getServiceIp(), deployDTO.getServiceUserName(), deployDTO.getServiceUserPassword(), projectPath, deployDTO.getSimpleServiceUrl());
 
     }
 
@@ -93,7 +94,12 @@ public class DeployHandler {
             scriptFile.delete();
         }
         ScriptUtil.markScriptFile(scriptFileName, cmd);
-        Process process = Runtime.getRuntime().exec(scriptFileName);
+        Process process;
+        if (isWindows()) {
+            process = Runtime.getRuntime().exec(scriptFileName);
+        } else {
+            process = Runtime.getRuntime().exec("bash " + scriptFileName);
+        }
         InputStream in = process.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
         String line;
