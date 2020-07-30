72d340f9caf058bfae7af9319e410ca587646458
xieqiuxing <2994892782@qq.com>
2020-07-30 17:12:26 +0800

    新增用户登出功能 xqx

--- fastgo-web/src/main/java/com/bee/team/fastgo/controller/LoginController.java
+++ fastgo-web/src/main/java/com/bee/team/fastgo/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.bee.team.fastgo.controller;
 
+import com.bee.team.fastgo.common.CommonLoginValue;
 import com.bee.team.fastgo.service.user.UserBo;
 import com.bee.team.fastgo.vo.user.UserLoginReqVo;
 import com.spring.simple.development.core.annotation.base.NoLogin;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @ClassName LoginController
@@ -34,8 +35,18 @@ public class LoginController {
     @ApiOperation(value = "用户登录")
     @ValidHandler(key = "userLoginReqVo", value = UserLoginReqVo.class, isReqBody = false)
     @NoLogin
-    public ResBody userLogin(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @RequestBody UserLoginReqVo userLoginReqVo) {
+    public ResBody userLogin(HttpServletRequest httpServletRequest, @RequestBody UserLoginReqVo userLoginReqVo) {
         userBo.login(httpServletRequest, userLoginReqVo.getUserName(), userLoginReqVo.getPassword());
+        return new ResBody().buildSuccessResBody(userLoginReqVo.getUserName());
+    }
+
+    @RequestMapping(value = "/loginOut", method = RequestMethod.GET)
+    @ApiOperation(value = "用户登出")
+    public ResBody userLoginOut(HttpServletRequest request) {
+        // 将信息保存到session
+        HttpSession session = request.getSession();
+        // 如果不存在session就新增session
+        session.removeAttribute(CommonLoginValue.SESSION_LOGIN_KEY);
         return new ResBody().buildSuccessResBody();
     }
 
