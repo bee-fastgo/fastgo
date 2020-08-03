280e5580d26a381262ae2da5e24cc4e90a00df3d
root <restart@2020>
2020-08-03 18:27:52 +0800

    git maven执行日志

--- fastgo-tools/src/main/java/com/bee/team/fastgo/tools/deploy/mvn/MavenUtil.java
+++ fastgo-tools/src/main/java/com/bee/team/fastgo/tools/deploy/mvn/MavenUtil.java
@@ -1,12 +1,9 @@
 package com.bee.team.fastgo.tools.deploy.mvn;
 
-import ch.ethz.ssh2.StreamGobbler;
 import com.bee.team.fastgo.tools.deploy.DeployHandler;
 import com.bee.team.fastgo.tools.log.DeployJobFileAppender;
 import com.spring.simple.development.support.properties.PropertyConfigurer;
 import org.apache.maven.shared.invoker.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -30,18 +27,15 @@ public class MavenUtil {
         request.setJavaHome(new File(PropertyConfigurer.getProperty("java.home")));
         Invoker invoker = new DefaultInvoker();
         invoker.setMavenHome(new File(PropertyConfigurer.getProperty("maven.home")));
-        InputStream inputStream = request.getInputStream(new FileInputStream(new File(DeployHandler.logPathThreadLocal.get())));
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        while (true) {
-            String line = br.readLine();
-            if (line == null) {
-                break;
-            }
-            DeployJobFileAppender.appendLog(DeployHandler.logPathThreadLocal.get(),line);
-        }
+
+        PrintStream out = new PrintStream(new FileOutputStream(new File(DeployHandler.logPathThreadLocal.get())), true);
+        InvokerLogger invokerLogger = new PrintStreamLogger(out,1);
+        invoker.setLogger(invokerLogger);
         if (invoker.execute(request).getExitCode() != 0) {
             return false;
         }
+        DeployJobFileAppender.appendLog(DeployHandler.logPathThreadLocal.get(), out.toString());
+
         return true;
     }
 }
