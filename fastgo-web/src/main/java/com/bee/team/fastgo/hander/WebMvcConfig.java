67498c85d3f0370a96c34006f3e913de2558d0a9
root <restart@2020>
2020-08-05 17:37:57 +0800

    资源地址过滤

--- fastgo-web/src/main/java/com/bee/team/fastgo/hander/WebMvcConfig.java
+++ fastgo-web/src/main/java/com/bee/team/fastgo/hander/WebMvcConfig.java
@@ -1,10 +1,13 @@
 package com.bee.team.fastgo.hander;
 
+import com.bee.team.fastgo.interceptor.FastGoInterceptor;
+import com.spring.simple.development.core.component.shiro.cas.ShiroLavaSupportInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,6 +28,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(pathPatterns).addResourceLocations("file:" + fileRootPath);
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new FastGoInterceptor()).addPathPatterns("/**").excludePathPatterns(pathPatterns);
     }
 
     @Bean(name="multipartResolver")
