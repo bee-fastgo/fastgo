0e88f5523ef032721704c96798e43477186828f3
hs <1500509412@qq.com>
2020-08-06 11:16:55 +0800

    bug fixed

--- fastgo-web/src/main/java/com/bee/team/fastgo/vo/project/ProjectDeployResVo.java
+++ fastgo-web/src/main/java/com/bee/team/fastgo/vo/project/ProjectDeployResVo.java
@@ -14,19 +14,22 @@ import lombok.Data;
 @ApiModel(value = "projectDeployResVo",description = "项目部署记录vo")
 public class ProjectDeployResVo {
 
-    @ApiModelProperty(value = "分支名称", example = "2020-06-06")
+    @ApiModelProperty(value = "分支名称", example = "master")
     private String branchName;
 
-    @ApiModelProperty(value = "项目名称", example = "2020-06-06")
+    @ApiModelProperty(value = "项目名称", example = "test-project")
     private String projectName;
 
-    @ApiModelProperty(value = "执行人", example = "2020-06-06")
+    @ApiModelProperty(value = "执行人", example = "test")
     private String user;
 
-    @ApiModelProperty(value = "用户成员", example = "2020-06-06")
+    @ApiModelProperty(value = "部署时间", example = "2020-06-06")
     private String gmtCreate;
 
-    @ApiModelProperty(value = "部署日志id", example = "2020-06-06")
+    @ApiModelProperty(value = "部署日志id", example = "sasdasd")
     private String deployLogId;
+
+    @ApiModelProperty(value = "部署状态：4-项目部署中；5-项目部署完成；7-项目部署失败", example = "4")
+    private Integer projectDeployStatus;
 
 }
