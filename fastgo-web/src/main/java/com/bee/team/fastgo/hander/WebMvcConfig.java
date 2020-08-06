0307777bcac6bf5adaf4de6f4319f07888d79956
root <restart@2020>
2020-08-06 16:54:18 +0800

    用户过滤器实现

--- fastgo-web/src/main/java/com/bee/team/fastgo/hander/WebMvcConfig.java
+++ fastgo-web/src/main/java/com/bee/team/fastgo/hander/WebMvcConfig.java
@@ -1,13 +1,10 @@
 package com.bee.team.fastgo.hander;
 
-import com.bee.team.fastgo.interceptor.FastGoInterceptor;
-import com.spring.simple.development.core.component.shiro.cas.ShiroLavaSupportInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
